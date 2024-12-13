4efc0c9b7a2f284e105ad9a7740e1cbbdc6509e2
Romith Mahmood <tmromith@gmail.com>
2024-12-13 20:36:57 +0000

    Fixed issue with names not showing in excel

--- ucc_staff.py
+++ ucc_staff.py
@@ -9,12 +9,18 @@ soup = BeautifulSoup(page.text, 'html.parser')
 names = soup.findAll("span", attrs={"class":"name"})
 departments = soup.findAll("span",attrs={"class":"department"})
 
-file = open("UCC_name_department.csv","w")
-write = csv.writer(file)
+with open("UCC_name_department.csv","w",newline="") as f:
+    write = csv.writer(f)
 
-write.writerow(["NAME","DEPARTMENT"])
+    write.writerow(["NAME","DEPARTMENT"])
 
-for name,department in zip(names,departments):
-    print(name.text + " - " + department.text)
-    write.writerow([name.text,department.text])
-file.close()
+    for name,department in zip(names,departments):
+        
+        name_text = name.find("a").text.strip() if name.find("a") else name.text.strip()
+        # Extract department text
+        department_text = department.text.strip()
+        
+        print(name_text + " - " + department_text)
+
+        write.writerow([name_text,department_text])
+
