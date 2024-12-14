18e59c51c786f49f6e36b57a5fd221bf390bfcc0
Romith Mahmood <tmromith@gmail.com>
2024-12-14 14:36:46 +0000

    Seperate columns
    Made name,title,department in seperate columns.

--- ucc_staff.py
+++ ucc_staff.py
@@ -7,20 +7,24 @@ page = requests.get('https://www.cubsucc.com/faculty-directory/')
 soup = BeautifulSoup(page.text, 'html.parser')
 
 names = soup.findAll("span", attrs={"class":"name"})
+titles = soup.findAll("span",attrs={"class":"jobTitle"})
 departments = soup.findAll("span",attrs={"class":"department"})
 
 with open("UCC_name_department.csv","w",newline="") as f:
     write = csv.writer(f)
 
-    write.writerow(["NAME","DEPARTMENT"])
+    write.writerow(["NAME","TITLE","DEPARTMENT"])
 
-    for name,department in zip(names,departments):
+    for name,title,department in zip(names,titles,departments):
         
+        # looks at the "a" tag as it contains the name of the staff.
         name_text = name.find("a").text.strip() if name.find("a") else name.text.strip()
+        # Extract title text
+        title_text = title.text.strip()
         # Extract department text
         department_text = department.text.strip()
         
-        print(name_text + " - " + department_text)
+        print(name_text + " - " + title_text + " - " + department_text)
 
-        write.writerow([name_text,department_text])
+        write.writerow([name_text,title_text,department_text])
 
