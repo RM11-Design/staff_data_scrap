9fbba4bdd083cc556ba308e8f9827dcddb5bcbfb
Romith Mahmood <tmromith@gmail.com>
2024-12-14 15:37:22 +0000

    Commented

--- ucc_staff.py
+++ ucc_staff.py
@@ -6,6 +6,7 @@ page = requests.get('https://www.cubsucc.com/faculty-directory/')
 
 soup = BeautifulSoup(page.text, 'html.parser')
 
+# Looks at the "span" tag and extracts inside the span tag.
 names = soup.findAll("span", attrs={"class":"name"})         
 titles = soup.findAll("span",attrs={"class":"jobTitle"})
 departments = soup.findAll("span",attrs={"class":"department"})
@@ -17,13 +18,14 @@ with open("UCC_name_department.csv","w",newline="") as f:
 
     for name,title,department in zip(names,titles,departments):
         
-        # Extracts name text
+        # Extracts name text from <a> tag
         name_text = name.a.contents[0].strip()
         # Extract title text
         title_text = title.text.strip()
         # Extract department text
         department_text = department.text.strip()
         
+        # This formats in way thats easy to read.
         print(name_text + " - " + title_text + " - " + department_text)
 
         write.writerow([name_text,title_text,department_text])
